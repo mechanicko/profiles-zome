f71e78c7d2fd4b54dc2993ba3eedfae8f30d2e00
Guillem Cordoba <guillem.cordoba@gmail.com>
2020-05-01 19:15:44 +0200

    Added test

--- example-dna/test/index.js
+++ example-dna/test/index.js
@@ -18,11 +18,11 @@ process.on("unhandledRejection", (error) => {
   console.error("got unhandledRejection:", error);
 });
 
-const dnaPath = path.join(__dirname, "../dist/dna.dna.json");
+const dnaPath = path.join(__dirname, "../dist/example-dna.dna.json");
 
 const dna = Config.dna(dnaPath, "scaffold-test");
 const conductorConfig = Config.gen(
-  { transactor: dna },
+  { profiles: dna },
   {
     network: {
       type: "sim2h",
@@ -38,28 +38,15 @@ const orchestrator = new Orchestrator({
   },
 });
 
-function offerCredits(to, credits) {
+function setUsername(username) {
   return (caller) =>
-    caller.call("transactor", "transactor", "offer_credits", {
-      creditor_address: to,
-      amount: credits,
-      timestamp: Math.floor(Date.now() / 1000),
+    caller.call("profiles", "profiles", "set_username", {
+      username,
     });
 }
 
-function getCounterpartyBalance(transactionAddress) {
-  return (caller) =>
-    caller.call("transactor", "transactor", "get_counterparty_balance", {
-      transaction_address: transactionAddress,
-    });
-}
-
-function acceptOffer(transactionAddress, lastHeaderAddress) {
-  return (caller) =>
-    caller.call("transactor", "transactor", "accept_offer", {
-      transaction_address: transactionAddress,
-      approved_header_address: lastHeaderAddress,
-    });
+function getAllAgents() {
+  return (caller) => caller.call("profiles", "profiles", "get_all_agents", {});
 }
 
 orchestrator.registerScenario("description of example test", async (s, t) => {
@@ -68,72 +55,18 @@ orchestrator.registerScenario("description of example test", async (s, t) => {
     true
   );
 
-  const aliceAddress = alice.instance("transactor").agentAddress;
-  const bobAddress = bob.instance("transactor").agentAddress;
+  const aliceAddress = alice.instance("profiles").agentAddress;
+  const bobAddress = bob.instance("profiles").agentAddress;
 
-  let result = await offerCredits(bobAddress, 10)(alice);
-  await s.consistency();
+  let result = await getAllAgents()(alice);
+  t.equal(result.Ok.length, 0);
+
+  result = await setUsername("alice")(alice);
   t.ok(result.Ok);
-
-  let transactionAddress = result.Ok;
-
-  result = await getCounterpartyBalance(transactionAddress)(alice);
-  t.equal(result.Ok.balance, 0);
-  t.equal(result.Ok.executable, true);
-
-  result = await getCounterpartyBalance(transactionAddress)(bob);
-  t.equal(result.Ok.balance, 0);
-  t.equal(result.Ok.executable, true);
-
-  result = await acceptOffer(
-    transactionAddress,
-    result.Ok.last_header_address
-  )(bob); // Alice has -10, Bob has +10
   await s.consistency();
-  t.ok(result);
 
-  result = await offerCredits(bobAddress, 10)(alice);
-  await s.consistency();
-  t.ok(result.Ok);
-
-  transactionAddress = result.Ok;
-
-  result = await getCounterpartyBalance(transactionAddress)(alice);
-  t.equal(result.Ok.balance, 10);
-  t.equal(result.Ok.executable, true);
-
-  result = await getCounterpartyBalance(transactionAddress)(bob);
-  t.equal(result.Ok.balance, -10);
-  t.equal(result.Ok.executable, true);
-  t.equal(result.Ok.valid, true);
-
-  result = await acceptOffer(
-    transactionAddress,
-    result.Ok.last_header_address
-  )(bob); // Alice has -20, Bob has +20
-  await s.consistency();
-  t.ok(result);
-
-  result = await offerCredits(bobAddress, 81)(alice);
-  await s.consistency();
-  t.ok(result.Ok);
-
-  transactionAddress = result.Ok;
-
-  result = await getCounterpartyBalance(transactionAddress)(bob);
-  t.equal(result.Ok.balance, -20);
-  t.equal(result.Ok.executable, false);
-  t.equal(result.Ok.valid, true);
-
-  result = await acceptOffer(
-    transactionAddress,
-    result.Ok.last_header_address
-  )(bob);
-  await s.consistency();
-  t.notOk(result.Ok);
-
-  result = await getCounterpartyBalance(transactionAddress)(bob);
-  t.notOk(result.Ok);
+  result = await getAllAgents()(bob);
+  t.equal(result.Ok.length, 1);
 });
 
 orchestrator.run();
